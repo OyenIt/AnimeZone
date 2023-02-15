5e881596e77d1903a9193227c29d01c5f67985fb
TMexe404 <muhammadrivan888@gmail.com>
2023-02-15 17:20:30 +0700

    bismillah berhasil aamiin

--- AnimeZone/settings.py
+++ AnimeZone/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "frontend-zone/build/static"),
-    os.path.join(BASE_DIR, "static")]
+    ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
